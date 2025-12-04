335783ca8f64ce77f6977e99225382cf63dd86b2
Malik99-ism <s.picot@ecole-ipssi.net>
2025-12-04 13:54:32 +0100

    modifie accueil

---
+++ src/views/accueil.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "accueil.css">
+
+    <title>Acceuil</title>
+</head>
+<body>
+ 
+    <nav style="position:absolute; top:20px; left:20px;">
+        <button id="theme-toggle">🌙 Mode Sombre</button>
+    </nav>
+ 
+    <div class="landing-container">
+        <h1 class="landing-title">Salut ! C'est Quizzeo, une plateforme qui propose des quiz à tout les niveaux</h1>
+        <p class="landing-text">
+            La plateforme ultime pour créer, partager et jouer à des quiz en temps réel.
+            De l'ecole à l'entrepreunariat, rejoignez-nous !
+        </p>
+       
+        <div class="btn-group">
+            <a href="index.php?route=connexion" class="btn-landing btn-primary">Connexion</a>
+           
+            <a href="index.php?route=inscription" class="btn-landing btn-secondary">Inscription</a>
+
+            <a href="index.php?route=informationentreprise" class="btn-landing btn-secondary">informartion entreprise</a>
+            
+            <a href="index.php?route=informationutilisateur" class="btn-landing btn-secondary">Information utilisateur</a>
+
+            <a href="index.php?route=informationecole" class="btn-landing btn-secondary">Information ecole</a>
+
+            <a href="index.php?route=classement" class="btn-landing btn-secondary">Classement</a>
+        </div>
+    </div>
+ 
+    <script src="public/scriptJS/accueil.js"></script>
+</body>
+</html>

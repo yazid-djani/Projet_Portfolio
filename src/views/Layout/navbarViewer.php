cac7d3bfb5dcd18520bce42fc1e52188088bf969
yazid <ydjani@proton.me>
2026-02-07 16:03:24 +0100

    architecture MVC completer

--- src/views/Layout/navbarViewer.php
+++ src/views/Layout/navbarViewer.php
@@ -9,14 +9,5 @@
         <a href="#dev-panel" class="nav-link">Développement</a>
         <a href="#reseau-panel" class="nav-link">Réseau & Système</a>
 
-        <span style="color:#555">|</span>
-
-        <?php if (isset($_SESSION['user_id'])): ?>
-            <a href="index.php?route=admin" class="nav-link" style="color:var(--primary-color); font-weight:bold;">Console Admin</a>
-            <a href="index.php?route=deconnexion" class="nav-btn btn-logout">Déconnexion</a>
-        <?php else: ?>
-            <a href="index.php?route=admin" class="nav-link">Connexion</a>
-        <?php endif; ?>
-
     </div>
 </nav>
